dfe3aa04bf2cde237d37d99df7e9f2e3ddfd9f41
Tajerbashi <kamrantajerbashi@gmail.com>
2022-12-26 15:31:43 +0330

    Repository & Service Class for PlayList is Completed

---
+++ DAL/Services/PlayListRepository.cs
@@ -0,0 +1,97 @@
+﻿using DAL.Context;
+using DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Services
+{
+    public class PlayListRepository : IPlayListRepository
+    {
+        DbContexts DB;
+        public PlayListRepository(DbContexts db) 
+        {
+            this.DB=db;
+        }
+        public bool Add(PlayList playList)
+        {
+            try
+            {
+                DB.PlayLists.Add(playList);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(PlayList playList)
+        {
+            try
+            {
+                DB.Entry(playList).State=EntityState.Deleted;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                PlayList play=DB.PlayLists.Find(id);
+                this.Delete(play);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            DB.Dispose();
+        }
+
+        public IEnumerable<PlayList> GetAll()
+        {
+            return DB.PlayLists.ToList();
+        }
+
+        public PlayList GetById(int id)
+        {
+            return DB.PlayLists.Find(id);
+        }
+
+        public PlayList GetByName(string Name)
+        {
+            return DB.PlayLists.Where(c => c.Name==Name).FirstOrDefault();
+        }
+
+        public void Save()
+        {
+            DB.SaveChanges();
+        }
+
+        public bool Update(PlayList playList)
+        {
+            try
+            {
+                DB.Entry(playList).State= EntityState.Modified;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
